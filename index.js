e6ef8c29f47ac3309d90cd18b9efa29b45e341f4
akshit2611 <akshit.26112002@gmail.com>
2025-07-16 13:14:01 +0530

    updated

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express=require("express");
 const app=express();
+
 require("dotenv").config();
 const port = process.env.PORT || 4000;
+
 const mongo=require("mongoose");
 const path=require("path")
 const datamodel=require("./datamodel");
 const bcrypt=require("bcrypt");
 const ejs=require("ejs");
-const jwt=require("jsonwebtoken");
+const jwt=require("jsonwebtoken")
+;
 const cookieParser = require("cookie-parser");
 const { log } = require("console");
 
 app.set("view engine","ejs")
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"public")))
